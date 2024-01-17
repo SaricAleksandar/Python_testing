d1c86698936c7a33a5ab4656fa7c2d2ab7d99223
SaricAleksandar <156901713+SaricAleksandar@users.noreply.github.com>
2024-01-17 16:45:07 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,2 +1,19 @@
 import os
+import subprocess
+
+
+def run_command(command):
+    try:
+        result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
+        print("Command output:")
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {e}")
+        print("Command output:")
+        print(e.output)
+
+
+command_to_run = r'"C:\Users\sesa744271\Documents\TestComplete 15 Projects\Analyse_Build\Analyse_Build.pjs"/run'
+print(command_to_run)
+run_command(command_to_run)
 print("Hello World")
