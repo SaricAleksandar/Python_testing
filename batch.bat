55f6c1d45f86b2919beb516b5bbd2b3a3f7cadca
SaricAleksandar <156901713+SaricAleksandar@users.noreply.github.com>
2024-04-04 07:01:20 -0700

    Update batch.bat

--- batch.bat
+++ batch.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\SmartBear\TestComplete 15\x64\Bin\TestComplete.exe" "C:\Work\My Projects\MySuite\MySuite.pjs" /r
+ECHO "C:\Program Files (x86)\SmartBear\TestComplete 15\x64\Bin\TestComplete.exe" "C:\Work\My Projects\MySuite\MySuite.pjs" /r
